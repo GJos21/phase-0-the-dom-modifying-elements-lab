fbd3bc31aae1d092a6d3bfb6b257d2cba717ca5d
Greg Schmitt <gregjschmitt@gmail.com>
2021-12-03 10:55:32 -0500

    completed assignment again

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.textContent = "YOUR-NAME is the champion";
+newHeader.id = "victory";
+
+// document.body.append(newHeader);
+
+
